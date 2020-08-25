95af08118678d3284f663ca585e44495c62fe8c5
HoldMySoul <martinshgj@inbox.lv>
2020-08-25 23:04:50 +0300

    Nuking Samsung hotplug and going up to V3.1. A3 charging current limit is now 1.5A (minor edits to pruh.sh)

--- pruh.sh
+++ pruh.sh
@@ -9,9 +9,9 @@ export USE_CCACHE=1
 CPU=`nproc --all`
 clear
 echo "Select"
-echo "1 = Clean Build"
-echo "2 = A320X"
-echo "3 = Exit"
+echo "1 = Exit"
+echo "2 = Clean build"
+echo "3 = A320X"
 echo "4 = J7velte"
 echo "5 = A6"
 echo "6 = J5" 
@@ -21,95 +21,119 @@ echo "9 = J7y17lte"
 echo "10 = on7xelte"
 read n
 clear  
-if [ $n -eq 1 ]; then
+elif [ $n -eq 1 ]; then
+exit
+if [ $n -eq 2 ]; then
 
 make clean && make mrproper
 rm -r /home/kernel/user/Pruh_Kernel/arch/arm64/boot/Image
 rm -r /home/kernel/user/Pruh_Kernel/arch/arm64/boot/dtb.img
-elif [ $n -eq 2 ]; then
+elif [ $n -eq 3 ]; then
 
-echo "=========="
-
-echo "=========="
 echo "============="
 echo "Building zImage For A320X"
 echo "============="
 make exynos7870-a3y17lte_defconfig
 make -j"$CPU"
 echo "Kernel Compiled for A3lte"
-cp -r /home/kernel/user/Pruh_Kernel/arch/arm64/boot/Image /home/kernel/user/Pruh_Kernel/build_folder/split_img/boot.img-zImage
-cp -r /home/kernel/user/Pruh_Kernel/arch/arm64/boot/dtb.img /home/kernel/user/Pruh_Kernel/build_folder/split_img/boot.img-dt
+mv /home/kernel/user/Pruh_Kernel/arch/arm64/boot/Image /home/kernel/user/Pruh_Kernel/build_folder/split_img/boot.img-zImage
+mv /home/kernel/user/Pruh_Kernel/arch/arm64/boot/dtb.img /home/kernel/user/Pruh_Kernel/build_folder/split_img/boot.img-dt
 echo martins | sudo -S /home/kernel/user/Pruh_Kernel/build_folder/repackimg.sh
 mv /home/kernel/user/Pruh_Kernel/build_folder/image-new.img /home/kernel/user/Pruh_Kernel/build_folder/output/boot_for_a3y17lte.img
-elif [ $n -eq 3 ]; then
-exit
+echo "Cleaning Please wait....."
+make clean && make mrproper
+echo "Done"
+echo "Flashable image is at output folder"
+
 elif [ $n -eq 4 ]; then
 
 make exynos7870-j7velte_defconfig
 make -j"$CPU"
 echo "Kernel Compiled for J7velte"
-cp -r /home/kernel/user/Pruh_Kernel/arch/arm64/boot/Image /home/kernel/user/Pruh_Kernel/build_folder/split_img/boot.img-zImage
-cp -r /home/kernel/user/Pruh_Kernel/arch/arm64/boot/dtb.img /home/kernel/user/Pruh_Kernel/build_folder/split_img/boot.img-dt
+mv /home/kernel/user/Pruh_Kernel/arch/arm64/boot/Image /home/kernel/user/Pruh_Kernel/build_folder/split_img/boot.img-zImage
+mv /home/kernel/user/Pruh_Kernel/arch/arm64/boot/dtb.img /home/kernel/user/Pruh_Kernel/build_folder/split_img/boot.img-dt
 echo martins | sudo -S /home/kernel/user/Pruh_Kernel/build_folder/repackimg.sh
 mv /home/kernel/user/Pruh_Kernel/build_folder/image-new.img /home/kernel/user/Pruh_Kernel/build_folder/output/boot_for_j7velte.img
-
+echo "Cleaning Please wait....."
+make clean && make mrproper
+echo "Done"
+echo "Flashable image is at output folder"
 elif [ $n -eq 5 ]; then
 
 make exynos7870-a6lte_defconfig
 make -j"$CPU"
 echo "Kernel Compiled for A6"
-cp -r /home/kernel/user/Pruh_Kernel/arch/arm64/boot/Image /home/kernel/user/Pruh_Kernel/build_folder/split_img/boot.img-zImage
-cp -r /home/kernel/user/Pruh_Kernel/arch/arm64/boot/dtb.img /home/kernel/user/Pruh_Kernel/build_folder/split_img/boot.img-dt
+mv /home/kernel/user/Pruh_Kernel/arch/arm64/boot/Image /home/kernel/user/Pruh_Kernel/build_folder/split_img/boot.img-zImage
+mv /home/kernel/user/Pruh_Kernel/arch/arm64/boot/dtb.img /home/kernel/user/Pruh_Kernel/build_folder/split_img/boot.img-dt
 echo martins | sudo -S /home/kernel/user/Pruh_Kernel/build_folder/repackimg.sh
 mv /home/kernel/user/Pruh_Kernel/build_folder/image-new.img /home/kernel/user/Pruh_Kernel/build_folder/output/boot_for_a6lte.img
-
+echo "Cleaning Please wait....."
+make clean && make mrproper
+echo "Done"
+echo "Flashable image is at output folder"
 elif [ $n -eq 6 ]; then
 
 make exynos7870-j5y17lte_defconfig
 make -j"$CPU"
 echo "Kernel Compiled for J5"
-cp -r /home/kernel/user/Pruh_Kernel/arch/arm64/boot/Image /home/kernel/user/Pruh_Kernel/build_folder/split_img/boot.img-zImage
-cp -r /home/kernel/user/Pruh_Kernel/arch/arm64/boot/dtb.img /home/kernel/user/Pruh_Kernel/build_folder/split_img/boot.img-dt
+mv /home/kernel/user/Pruh_Kernel/arch/arm64/boot/Image /home/kernel/user/Pruh_Kernel/build_folder/split_img/boot.img-zImage
+mv /home/kernel/user/Pruh_Kernel/arch/arm64/boot/dtb.img /home/kernel/user/Pruh_Kernel/build_folder/split_img/boot.img-dt
 echo martins | sudo -S /home/kernel/user/Pruh_Kernel/build_folder/repackimg.sh
 mv /home/kernel/user/Pruh_Kernel/build_folder/image-new.img /home/kernel/user/Pruh_Kernel/build_folder/output/boot_for_j5y17lte.img
-
+echo "Cleaning Please wait....."
+make clean && make mrproper
+echo "Done"
+echo "Flashable image is at output folder"
 elif [ $n -eq 7 ]; then
 
 make exynos7870-j6lte_defconfig
 make -j"$CPU"
 echo "Kernel Compiled for J6lte"
-cp -r /home/kernel/user/Pruh_Kernel/arch/arm64/boot/Image /home/kernel/user/Pruh_Kernel/build_folder/split_img/boot.img-zImage
-cp -r /home/kernel/user/Pruh_Kernel/arch/arm64/boot/dtb.img /home/kernel/user/Pruh_Kernel/build_folder/split_img/boot.img-dt
+mv /home/kernel/user/Pruh_Kernel/arch/arm64/boot/Image /home/kernel/user/Pruh_Kernel/build_folder/split_img/boot.img-zImage
+mv /home/kernel/user/Pruh_Kernel/arch/arm64/boot/dtb.img /home/kernel/user/Pruh_Kernel/build_folder/split_img/boot.img-dt
 echo martins | sudo -S /home/kernel/user/Pruh_Kernel/build_folder/repackimg.sh
 mv /home/kernel/user/Pruh_Kernel/build_folder/image-new.img /home/kernel/user/Pruh_Kernel/build_folder/output/boot_for_j6lte.img
-
+echo "Cleaning Please wait....."
+make clean && make mrproper
+echo "Done"
+echo "Flashable image is at output folder"
 elif [ $n -eq 8 ]; then
 
 make exynos7870-j7xelte_defconfig
 make -j"$CPU"
 echo "Kernel Compiled for J7xelte"
-cp -r /home/kernel/user/Pruh_Kernel/arch/arm64/boot/Image /home/kernel/user/Pruh_Kernel/build_folder/split_img/boot.img-zImage
-cp -r /home/kernel/user/Pruh_Kernel/arch/arm64/boot/dtb.img /home/kernel/user/Pruh_Kernel/build_folder/split_img/boot.img-dt
+mv /home/kernel/user/Pruh_Kernel/arch/arm64/boot/Image /home/kernel/user/Pruh_Kernel/build_folder/split_img/boot.img-zImage
+mv /home/kernel/user/Pruh_Kernel/arch/arm64/boot/dtb.img /home/kernel/user/Pruh_Kernel/build_folder/split_img/boot.img-dt
 echo martins | sudo -S /home/kernel/user/Pruh_Kernel/build_folder/repackimg.sh
 mv /home/kernel/user/Pruh_Kernel/build_folder/image-new.img /home/kernel/user/Pruh_Kernel/build_folder/output/boot_for_j7xelte.img
-
+echo "Cleaning Please wait....."
+make clean && make mrproper
+echo "Done"
+echo "Flashable image is at output folder"
 elif [ $n -eq 9 ]; then
 
 make exynos7870-j7y17lte_defconfig
 make -j"$CPU"
 echo "Kernel Compiled for J7y17lte"
-cp -r /home/kernel/user/Pruh_Kernel/arch/arm64/boot/Image /home/kernel/user/Pruh_Kernel/build_folder/split_img/boot.img-zImage
-cp -r /home/kernel/user/Pruh_Kernel/arch/arm64/boot/dtb.img /home/kernel/user/Pruh_Kernel/build_folder/split_img/boot.img-dt
+mv /home/kernel/user/Pruh_Kernel/arch/arm64/boot/Image /home/kernel/user/Pruh_Kernel/build_folder/split_img/boot.img-zImage
+mv /home/kernel/user/Pruh_Kernel/arch/arm64/boot/dtb.img /home/kernel/user/Pruh_Kernel/build_folder/split_img/boot.img-dt
 echo martins | sudo -S /home/kernel/user/Pruh_Kernel/build_folder/repackimg.sh
 mv /home/kernel/user/Pruh_Kernel/build_folder/image-new.img /home/kernel/user/Pruh_Kernel/build_folder/output/boot_for_j7y17lte.img
-
+echo "Cleaning Please wait....."
+make clean && make mrproper
+echo "Done"
+echo "Flashable image is at output folder"
 elif [ $n -eq 10 ]; then
 
 make exynos7870-on7xelte_defconfig
 make -j"$CPU"
 echo "Kernel Compiled for on7xelte"
-cp -r /home/kernel/user/Pruh_Kernel/arch/arm64/boot/Image /home/kernel/user/Pruh_Kernel/build_folder/split_img/boot.img-zImage
-cp -r /home/kernel/user/Pruh_Kernel/arch/arm64/boot/dtb.img /home/kernel/user/Pruh_Kernel/build_folder/split_img/boot.img-dt
+mv /home/kernel/user/Pruh_Kernel/arch/arm64/boot/Image /home/kernel/user/Pruh_Kernel/build_folder/split_img/boot.img-zImage
+mv /home/kernel/user/Pruh_Kernel/arch/arm64/boot/dtb.img /home/kernel/user/Pruh_Kernel/build_folder/split_img/boot.img-dt
 echo martins | sudo -S /home/kernel/user/Pruh_Kernel/build_folder/repackimg.sh
 mv /home/kernel/user/Pruh_Kernel/build_folder/image-new.img /home/kernel/user/Pruh_Kernel/build_folder/output/boot_for_on7xelte.img
+echo "Cleaning Please wait....."
+make clean && make mrproper
+echo "Done"
+echo "Flashable image is at output folder"
 fi
